051594ce2b92232259c05c8a13bcaf4fe7cefc6c
Christoph Dorn <christoph@christophdorn.com>
2017-05-22 23:26:21 -0700

    wip

--- examples/01-HelloWorld/main.sh
+++ examples/01-HelloWorld/main.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash.origin.script
 
 BO_run_recent_node --eval '
+    const ASSERT = require("assert");
+    const RUNBASH = require("../..");
 
-const ASSERT = require("assert");
-const RUNBASH = require("../..");
+    RUNBASH([
+        "echo $PWD"
+    ]).then(function (result) {
 
-RUNBASH([
-    "echo $PWD"
-]).then(function (result) {
+        ASSERT.equal(result.stdout, process.cwd() + "\n");
 
-    ASSERT.equal(result.stdout, process.cwd() + "\n");
+        console.log("result", JSON.stringify(result, null, 4));
 
-    console.log("result", JSON.stringify(result, null, 4));
+        process.stdout.write("OK\n");
+        process.exit(0);
 
-    process.stdout.write("OK\n");
-    process.exit(0);
+    }).catch(function (err) {
 
-}).catch(function (err) {
-
-    console.error(err.stack);
-    process.exit(1);
-});
+        console.error(err.stack);
+        process.exit(1);
+    });
 '
 
 echo "OK"
